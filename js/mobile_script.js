2eebae74f907b42a4d2a76257ab8da2ca1d57b61
DeversGreenGV <86885053+DeversGreenGV@users.noreply.github.com>
2022-06-21 13:38:32 +0300

    Team block mobile(v 0.2)

--- js/mobile_script.js
+++ js/mobile_script.js
@@ -1,12 +1,20 @@
 const Team__item = document.querySelectorAll(".Team__item");
+const Team_members_cont = document.querySelector(".Team__members__conts .left__cont");
+const Team_members_content = Team_members_cont.innerHTML;
+console.log(Team_members_content);
 
-window.addEventListener("resize",ChangeLayout);
+window.addEventListener("loadeddata",ChangeLayout);
 function ChangeLayout(){
     if (window.innerWidth<= 991.98) {
-        console.log(window.innerWidth+" "+window.innerHeight);
         Team__item.forEach((el, index) => {
-            // console.log(element);
+            let Item__members = el.querySelector('.item__members');
             el.classList.add('item__border_color'+(index+1));
+            Item__members.insertAdjacentHTML("afterbegin",Team_members_content);
+            let Item__text= Item__members.querySelector('.Text');
+            Item__text.textContent = Item__text.textContent.slice(0,380)+'...';
+            // Item__text.
+            // console.log(Item__members.querySelector('.Text').textContent.length);
+            // Item__members.querySelector('.Text').textContent
         });
     }
     
