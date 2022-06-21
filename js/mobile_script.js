bfe726c64e898a0489f6ac827e683ded48c6ec9f
DeversGreenGV <86885053+DeversGreenGV@users.noreply.github.com>
2022-06-21 18:35:18 +0300

    Team block mobile(v 0.3)+other

--- js/mobile_script.js
+++ js/mobile_script.js
@@ -1,22 +1,27 @@
 const Team__item = document.querySelectorAll(".Team__item");
 const Team_members_cont = document.querySelector(".Team__members__conts .left__cont");
 const Team_members_content = Team_members_cont.innerHTML;
+const AboutUS_list = document.querySelector(".AboutUS__list");
 console.log(Team_members_content);
 
-window.addEventListener("loadeddata",ChangeLayout);
+window.addEventListener("loadeddata",ChangeLayout());
+window.addEventListener("resize",ChangeLayout,true);
 function ChangeLayout(){
     if (window.innerWidth<= 991.98) {
         Team__item.forEach((el, index) => {
             let Item__members = el.querySelector('.item__members');
-            el.classList.add('item__border_color'+(index+1));
+            Item__members.innerHTML ='';
             Item__members.insertAdjacentHTML("afterbegin",Team_members_content);
             let Item__text= Item__members.querySelector('.Text');
             Item__text.textContent = Item__text.textContent.slice(0,380)+'...';
-            // Item__text.
+            
+            // // Item__text.
             // console.log(Item__members.querySelector('.Text').textContent.length);
             // Item__members.querySelector('.Text').textContent
         });
+        AboutUS_list.classList.remove('fd-row');
+        AboutUS_list.classList.add('fd-col');
     }
     
 }
-ChangeLayout();
+// ChangeLayout();
